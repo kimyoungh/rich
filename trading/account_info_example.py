a90358b265723003ef618912a7bdcb5b191842b6
kimoyungh <eluster107@gmail.com>
2023-07-07 19:11:11 +0900

    add account info example

---
+++ trading/account_info_example.py
@@ -0,0 +1,19 @@
+"""
+    Account Information(KIS)
+
+    한투 API 계정 정보 예시 코드로,
+    반드시 account_info.py 이름으로
+    아래 KIS_ACCONT_INFO 알맞게 기입한 코드를 저장해야
+    오류 없이 작동 가능함
+
+    아래 key값들은 한투 API 공식 홈페이지 참고하여 방법 숙지 및 작성할 것
+"""
+
+KIS_ACCOUNT_INFO = {
+    'app_key': "",
+    'app_secret': "",
+    'dom_stk_acnt': '',
+    'dom_stk_acnt_prdt_cd': '',
+    'dom_deri_acnt': '',
+    'dom_deri_acnt_prdt_cd': '',
+}
